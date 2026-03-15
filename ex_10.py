bc49dd04eab14bf3d65658b627b9ef4a24aa1340
Aleks "F1SZTE <u182_mocale_waw@technischools.com>
2026-03-15 17:11:24 +0100

    Poprawa zadania 10

--- ex_10.py
+++ ex_10.py
@@ -2,3 +2,5 @@ liczba = int(input("Podaj liczbę dziesiętną: "))
 
 print("System szesnastkowy:", hex(liczba)[2:])
 print("System ósemkowy:", oct(liczba)[2:])
+
+# [2:] usuwa prefiks 0x i 0o dodany przez finkcje hex i oct (tak samo jak przez bin)
